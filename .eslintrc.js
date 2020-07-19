6e82808288d1b478f8fcb659be1e83182f933376
icetomcat <icetomcat@gmail.com>
2020-07-19 12:22:57 +0700

    fix ESLint: Cannot read property 'loc' of undefined

--- .eslintrc.js
+++ .eslintrc.js
@@ -67,6 +67,7 @@ module.exports = {
 
   // add your custom rules here
   rules: {
+    '@typescript-eslint/indent': ['error', 2],
     // allow async-await
     'generator-star-spacing': 'off',
     // allow paren-less arrow functions
